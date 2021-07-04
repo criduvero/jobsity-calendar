250074a554f532008be84935f551d56c221e9f59
Cristian Velásquez <criduvero@gmail.com>
2021-07-04 16:41:16 -0500

    reminder-list design improved

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,6 +7,7 @@ import { LaddaModule } from 'angular2-ladda';
 import { ColorPickerModule } from 'ngx-color-picker';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { OrderModule } from 'ngx-order-pipe';
 
 @NgModule({
   declarations: [],
@@ -18,6 +19,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     ColorPickerModule,
     NgbModule,
     FontAwesomeModule,
+    OrderModule
   ]
 })
 export class SharedModule { }
