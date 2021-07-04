d4596a2dfc9c2cad1e715c2f6a7a60f145eb4d99
Cristian Velásquez <criduvero@gmail.com>
2021-07-04 15:33:55 -0500

    design improved

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 
 import { CommonModule } from '@angular/common';
-import { ChunkPipe } from '../pipes/chunk.pipe';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { LaddaModule } from 'angular2-ladda';
@@ -10,12 +9,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [
-    ChunkPipe
-  ],
-  imports: [
-    CommonModule
-  ],
+  declarations: [],
   exports: [
     CommonModule,
     LaddaModule,
@@ -24,7 +18,6 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     ColorPickerModule,
     NgbModule,
     FontAwesomeModule,
-    ChunkPipe
   ]
 })
 export class SharedModule { }
